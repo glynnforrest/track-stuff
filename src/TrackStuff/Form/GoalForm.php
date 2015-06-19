b9d48b57ea8e1c41cb7ce8f9cdd0cbf3f7469067
Glynn Forrest <me@glynnforrest.com>
2015-06-19 08:06:19 +0100

    Add GoalForm.

---
+++ src/TrackStuff/Form/GoalForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TrackStuff\Form;
+
+use Reform\Form\Form;
+use Reform\Form\Renderer\FoundationRenderer;
+
+/**
+ * GoalForm
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class GoalForm extends Form
+{
+    protected function init()
+    {
+        parent::init();
+        $this->text('title');
+        $this->submit('Save');
+        $this->setDefaultRenderer(new FoundationRenderer());
+    }
+}
