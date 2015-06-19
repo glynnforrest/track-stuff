d50cc1d3d2f5d7cb811bd7be43e9a3f72d6c37d2
Glynn Forrest <me@glynnforrest.com>
2015-06-19 08:17:07 +0100

    Add flash messages to base template.

--- app/Application.php
+++ app/Application.php
@@ -18,10 +18,17 @@ class Application
         $neptune->addModule(new AssetsModule(false));
 
         $neptune->addService(new Service\RoutingService());
+        $neptune->addService(new Service\SessionService());
         $neptune->addService(new Service\TwigService());
         $neptune->addService(new Service\DatabaseService());
         $neptune->addService(new Service\MonologService());
         $neptune->addService(new Service\FormService());
+
+        $neptune->extend('twig', function($twig, $neptune) {
+            $twig->addGlobal('app', $neptune);
+
+            return $twig;
+        });
     }
 
     public function console(Neptune $neptune)
