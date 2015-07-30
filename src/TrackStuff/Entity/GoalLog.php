01e1547ccc65776796561d581ba34bc3435092f2
Glynn Forrest <me@glynnforrest.com>
2015-07-30 08:49:23 +0100

    Adding simple GoalLog entity.

---
+++ src/TrackStuff/Entity/GoalLog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TrackStuff\Entity;
+
+use ActiveDoctrine\Entity\Entity;
+
+class GoalLog extends Entity
+{
+    protected static $table = 'goal_logs';
+    protected static $primary_key = 'id';
+    protected static $fields = [
+        'id',
+        'goal_id',
+        'date',
+        'amount'
+    ];
+    protected static $relations = [
+        'goal' => [
+            'belongs_to', 'TrackStuff\Entity\Goal', 'id', 'goal_id'
+        ],
+    ];
+}
