a1a1c223609ef588ce4252defdcf4e907a1d6fbc
Glynn Forrest <me@glynnforrest.com>
2015-07-30 08:50:02 +0100

    Add GoalLog migration.

---
+++ src/TrackStuff/Migrations/Migration20150622080534CreateGoalLogTable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace TrackStuff\Migrations;
+
+use Neptune\Database\Migration\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Migration20150622080534CreateGoalLogTable extends AbstractMigration
+{
+    protected $description = 'Create goal log table';
+
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('goal_logs');
+
+        $id = $table->addColumn('id', 'integer', ['unsigned' => true]);
+        $id->setAutoIncrement(true);
+        $table->setPrimaryKey(['id']);
+
+        $table->addColumn('date', 'datetime');
+        $table->addColumn('goal_id', 'integer', ['unsigned' => true]);
+        $table->addColumn('amount', 'integer', ['unsigned' => true]);
+
+        return $schema;
+    }
+
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('goal_logs');
+    }
+
+}
