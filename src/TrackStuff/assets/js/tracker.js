54540b671cfe7382fda5b84e16f5463cdffd22ac
Glynn Forrest <me@glynnforrest.com>
2015-12-20 10:45:28 +0000

    Up and down arrows increment/decrement the date

--- src/TrackStuff/assets/js/tracker.js
+++ src/TrackStuff/assets/js/tracker.js
@@ -3,7 +3,31 @@ $(function () {
     format: 'yyyy/m/d'
   });
 
+  function previousDay() {
+    var datepicker = $('.tracker-date');
+    var date = new Date(datepicker.val());
+    date.setDate(date.getDate() - 1);
+    datepicker.fdatepicker('update', date);
+  }
+
+  function nextDay() {
+    var datepicker = $('.tracker-date');
+    var date = new Date(datepicker.val());
+    date.setDate(date.getDate() + 1);
+    datepicker.fdatepicker('update', date);
+  }
+
   $('.tracker-input').keydown(function(e) {
+    //keyup
+    if (e.keyCode === 38) {
+      previousDay();
+    }
+
+    //keydown
+    if (e.keyCode === 40) {
+      nextDay();
+    }
+
     if (e.keyCode === 13) {
       var text = $(this).val();
       if (text.trim() === '') {
