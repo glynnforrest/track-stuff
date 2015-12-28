dd56007c01d40322ad5167f2d8a306a3159ea197
Glynn Forrest <me@glynnforrest.com>
2015-12-28 21:27:18 +0000

    Adding timestamps to GoalLog

--- src/TrackStuff/Entity/GoalLog.php
+++ src/TrackStuff/Entity/GoalLog.php
@@ -3,16 +3,21 @@
 namespace TrackStuff\Entity;
 
 use ActiveDoctrine\Entity\Entity;
+use ActiveDoctrine\Entity\Traits\TimestampTrait;
 
 class GoalLog extends Entity
 {
+    use TimestampTrait;
+
     protected static $table = 'goal_logs';
     protected static $primary_key = 'id';
     protected static $fields = [
         'id',
         'goal_id',
         'date',
-        'amount'
+        'amount',
+        'created_at',
+        'updated_at',
     ];
     protected static $relations = [
         'goal' => [
@@ -20,7 +25,10 @@ class GoalLog extends Entity
         ],
     ];
     protected static $types = [
+        'id' => 'integer',
         'date' => 'date',
         'amount' => 'integer',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 }
