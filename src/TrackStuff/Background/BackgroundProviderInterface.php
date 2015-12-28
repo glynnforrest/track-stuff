d72900be7ce71194e137a01a6b8c357a7f8fe901
Glynn Forrest <me@glynnforrest.com>
2015-12-28 22:59:01 +0000

    Add BackgroundProviderInterface

---
+++ src/TrackStuff/Background/BackgroundProviderInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TrackStuff\Background;
+
+/**
+ * BackgroundProviderInterface
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+interface BackgroundProviderInterface
+{
+    function getBackgroundUrl();
+}
