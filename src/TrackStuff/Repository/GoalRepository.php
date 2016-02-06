c1ac4cbf2628a728f3a8c5f8295e207b920d596a
Glynn Forrest <me@glynnforrest.com>
2016-02-06 23:00:59 +0000

    Add GoalRepository#getMode()

--- src/TrackStuff/Repository/GoalRepository.php
+++ src/TrackStuff/Repository/GoalRepository.php
@@ -3,6 +3,8 @@
 namespace TrackStuff\Repository;
 
 use ActiveDoctrine\Repository\AbstractRepository;
+use TrackStuff\Entity\Goal;
+use TrackStuff\Entity\GoalLog;
 
 /**
  * GoalRepository
@@ -12,4 +14,25 @@ use ActiveDoctrine\Repository\AbstractRepository;
 class GoalRepository extends AbstractRepository
 {
     protected $entity_class = 'TrackStuff\Entity\Goal';
+
+    public function getMode(Goal $goal)
+    {
+        $result = $this->conn->createQueryBuilder()
+                ->select('amount, COUNT(1) as count')
+                ->from(GoalLog::getTable())
+                ->where('goal_id = ?')
+                ->setParameter(0, $goal->id)
+                ->orderBy('count', 'DESC')
+                ->groupBy('amount')
+                ->setMaxResults(1)
+                ->execute()
+                ->fetch();
+
+        if (isset($result['amount'])) {
+            return $result['amount'];
+        }
+
+        return 0;
+
+    }
 }
